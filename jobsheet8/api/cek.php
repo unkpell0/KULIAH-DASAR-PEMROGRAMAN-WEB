75c68b1fc1430d0290cb982a37d602bd48b96af5
unkpell0 <marvelinohusca123@gmail.com>
2026-09-21 18:02:31 +0700

    Jobsheet 8 cek.php (2)

--- jobsheet8/api/cek.php
+++ jobsheet8/api/cek.php
@@ -1,1 +1,5 @@
-   <?php var_dump(getenv('DB_HOST'), $_ENV['DB_HOST'] ?? null);
+<?php
+header('Content-Type: text/plain');
+var_dump(getenv('DB_USER'));
+var_dump(strlen((string) getenv('DB_PASS')));
+var_dump(getenv('DB_PORT'), getenv('DB_NAME'), getenv('DB_SSLMODE'));
