6dc3fb937f763262bfbfbf62e78b608ebc6a9783
unkpell0 <marvelinohusca123@gmail.com>
2026-09-22 13:46:43 +0700

    change to transaction pooler

--- jobsheet8/api/includes/koneksi.php
+++ jobsheet8/api/includes/koneksi.php
@@ -8,7 +8,8 @@ $ssl  = getenv('DB_SSLMODE') ?: 'prefer';
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db;sslmode=$ssl", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 } catch (PDOException $e) {
     die("Koneksi database gagal: " . $e->getMessage());
 }
